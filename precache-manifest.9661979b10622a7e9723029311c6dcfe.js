76e2feb54f98d8af6f5e8607fad2b3611efbd569
Saveliy-Georgiy <savvabiryukov1@gmail.com>
2020-02-12 14:51:56 +0300

    Updates

---
+++ precache-manifest.9661979b10622a7e9723029311c6dcfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a42467be04ef695e2cb2a97e6be0278e",
+    "url": "/my_portfolio/index.html"
+  },
+  {
+    "revision": "72b52ba5b9935e65d9c2",
+    "url": "/my_portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ec38374331bc5ba3a014",
+    "url": "/my_portfolio/static/js/2.10bf1447.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my_portfolio/static/js/2.10bf1447.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72b52ba5b9935e65d9c2",
+    "url": "/my_portfolio/static/js/main.aabd4701.chunk.js"
+  },
+  {
+    "revision": "7d83385549fbee37eac6",
+    "url": "/my_portfolio/static/js/runtime-main.b6eebb5d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my_portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
